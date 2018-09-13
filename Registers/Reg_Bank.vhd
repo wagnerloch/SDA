772108b1977955cdc239c9eaa850800765371407
Wagner <wagner_loch@hotmail.com>
2018-09-13 20:12:55 -0300

    Banco de Registradores

--- Registers/Reg_Bank.vhd
+++ Registers/Reg_Bank.vhd
@@ -15,38 +15,45 @@ ENTITY Reg_Bank IS
 	);
 END Reg_Bank;
 
-ARCHITECTURE Reg_Bank_arq OF Reg_Bank IS
+ARCHITECTURE comportamento OF Reg_Bank IS
 
-	COMPONENT Reg32 PORT (
-		dados		: in	std_logic_vector (31 downto 0);
-		reset		: in	std_logic;
-		clk1		: in	std_logic;
-		saida		: out	std_logic_vector (31 downto 0));
+	COMPONENT RegN IS
+		GENERIC (N 		: integer := 32);
+		PORT (
+			entrada		: in	std_logic_vector (N-1 downto 0);
+			reset		: in	std_logic;
+			clock		: in	std_logic;
+			enable		: in 	std_logic;
+			saida		: out	std_logic_vector (N-1 downto 0)
+		);
 	END COMPONENT;
-	
-	--'entradas' e 'saidas', vetor de 31 posiçao, que representa os 31 "fios" que vão ligados, um em cada registrador do banco
-	type entradas_type 	is array (0 to 31) of std_logic_vector(31 downto 0);
-	type saidas_type		is array (0 to 31) of std_logic_vector(31 downto 0);
-	signal entradas  	: entradas_type;
+
+	--'saidas', vetor de 31 posiçao, que representa os 31 "fios" que vão ligados, um em cada registrador do banco
+	type saidas_type	is array (0 to 31) of std_logic_vector(31 downto 0);
 	signal saidas		: saidas_type;
+	signal enable		: std_logic_vector (31 downto 0);
 	
-	BEGIN	
+	BEGIN
+	
 		PROCESS (clk, regDestino, regFonte1, regFonte2)
 		BEGIN
-			entradas(to_integer(unsigned(regDestino))) <= dadoEntrada;
+			for i in 0 to 31 loop
+				enable(i) <= '0';
+			end loop;
+			enable(to_integer(unsigned(regDestino))) <= '1';
 			dadoSaida1 <= saidas(to_integer(unsigned(regFonte1)));
 			dadoSaida2 <= saidas(to_integer(unsigned(regFonte2)));
 		END PROCESS;
 		
-		--Gera os 32 registradores conectando seus fios de entrada e saída
-		GEN_REG:
-		for I in 0 to 31 generate
-			registradores	: Reg32 port map (
-				dados => entradas(to_integer(unsigned(regDestino))),
-				reset => reset,
-				clk1	=> clk,
-				saida => saidas(I)
-			);
-		end generate GEN_REG;
-	
-END Reg_Bank_arq;
+		GEN_REG: for i in 0 to 31 generate
+			REGISTRADOR : RegN
+				GENERIC MAP (N => 32)
+				PORT MAP (
+					entrada 	=> dadoEntrada,
+					reset		=> reset,
+					clock		=> clk,
+					enable	=> enable(i),
+					saida		=> saidas(i)
+				);
+		END generate GEN_REG;
+END comportamento;
